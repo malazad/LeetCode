bf21da30d1997e4a862f6eced46228a76e313d08
Md Washik Al Azad <37837507+malazad@users.noreply.github.com>
2023-07-26 14:50:09 -0400

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,31 +1,8 @@
-class Solution:
-    def isAnagram(self, s, t):
-        if len(s) != len(t):
-            return False
-        if sorted(s) == sorted(t):
-            return True
-        return False
-
-
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        final_list = []
-        string_dict = {len(s): [] for s in strs}
+        hashmap = dict()
         for s in strs:
-            string_dict[len(s)].append(s)
-
-        for x in string_dict:
-            strs = string_dict[x]
-            while len(strs) > 0:
-                temp_anagram_list = []
-                temp_anagram_list.append(strs[0])
-                pop_list = []
-                for i in range(1,len(strs)):
-                    if len(strs[0]) == len(strs[i]):
-                        if self.isAnagram(strs[0], strs[i]):
-                            temp_anagram_list.append(strs[i])
-                            pop_list.append(i)
-                for i in range(len(pop_list)-1, -1, -1):
-                    strs.pop(pop_list[i])
-                strs.pop(0)
-                final_list.append(temp_anagram_list)
-        return final_list
+            s_sorted = "".join(sorted(s))
+            if s_sorted not in hashmap.keys():
+                hashmap[str(s_sorted)] = []
+            hashmap[s_sorted] += [s]
+        return list(hashmap.values())
